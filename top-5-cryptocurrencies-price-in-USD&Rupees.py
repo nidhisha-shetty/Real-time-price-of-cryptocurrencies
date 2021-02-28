4ac3a5955661dd3ea78151b093c7e0baae4b610b
Nidhisha Shetty <shetty3491@gmail.com>
2021-02-28 16:06:08 +0530

    Update top-5-cryptocurrencies-price-in-USD&Rupees.py

--- top-5-cryptocurrencies-price-in-USD&Rupees.py
+++ top-5-cryptocurrencies-price-in-USD&Rupees.py
@@ -1,18 +1,19 @@
 import requests
 import json
 
-def cur_con(url):
-	response = requests.get(url)
-	response_json = json.loads(response.text)
-	b = response_json[0]["price_usd"]
-	return(b)
 
-list_url=["https://api.coinmarketcap.com/v1/ticker/bitcoin/",
-		      "https://api.coinmarketcap.com/v1/ticker/ethereum/",
-	        "https://api.coinmarketcap.com/v1/ticker/ripple/",
-		      "https://api.coinmarketcap.com/v1/ticker/litecoin/",
-		      "https://api.coinmarketcap.com/v1/ticker/factom/"]
+def cur_con(url):
+    response = requests.get(url)
+    response_json = json.loads(response.text)
+    return response_json[0]['current_price']
+    
+
+list_url=["https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin",
+	"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum",
+	"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ripple",
+	"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=litecoin",
+	"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=factom"]
 
 for url in list_url:
-  print("Price in USD: "+cur_con(url))
- 	print("Price in rupees: "+str(float(cur_con(url))*70)+"\n")
+    print("Price in USD: "+str(cur_con(url)))
+    print("Price in Rupees: "+str(float(cur_con(url))*70)+"\n")
